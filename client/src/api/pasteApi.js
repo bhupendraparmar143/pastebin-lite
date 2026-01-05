439f95a99a402f760a1d2f4e052efd6a395fe70e
Bhupendra <bhupiparmar1430@gmail.com>
2026-01-05 21:51:14 +0530

    change base path of render

--- client/src/api/pasteApi.js
+++ client/src/api/pasteApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_URL || '/api';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'https://pastebin-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
