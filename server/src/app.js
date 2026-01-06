8d8de12c96e7c8e0915bc58bd79b9f757b6cdc49
Bhupendra <bhupiparmar1430@gmail.com>
2026-01-06 14:01:45 +0530

    env changed

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,9 @@ app.use('/api/', limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: [process.env.FRONTEND_URL || 'http://localhost:3000',
+    "https://pastebin-lite-app-indol.vercel.app/"
+  ],
   credentials: true
 }));
 
